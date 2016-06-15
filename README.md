c7498e20a0cabb15043b667ba52b0330dc19acaf
Ian Paul <ian@ianpaul.net>
2016-06-15 18:41:39 +0300

    Initial commit.
    All the files.

---
+++ README.md
@@ -0,0 +1,21 @@
+#Chirpme
+Chirpme is a simple Slack slash command that retrieves the latest tweet of a specified username, and returns that tweet as a URL. Slack's Twitter app, if enabled, then takes over to display the actual tweet. If you don't supply a valid Twitter username you'll get an error message.
+
+The script is written in PHP and uses [James Mallison's PHP Wrapper for Twitter](https://github.com/J7mbo/twitter-api-php) to do all the OAuth dirty work on Twitter. 
+
+Credit goes to [David McCreath's Is It Up for Slack](https://github.com/mccreath/isitup-for-slack),  and [Ian Anderson Gray's First Twitter app](https://github.com/iagdotme/MyFirstTwitterApp) of which this script is an amalgam with minor additions by me.
+
+#Installation
+*TL;DR:* Run this script and [@J7mbo's wrapper](https://github.com/J7mbo/twitter-api-php) on a server with PHP and PHP cURL. Make sure your team is using Slack's built-in Twitter app for displaying tweets.
+
+To run Chirpme you'll need admin access to a Slack team (obviously) and the ability to create a new slash command for your team. To gain access to Twitter you'll also need a Twitter developer account and the appropriate access tokens and keys for a Twitter app. For more information on using Twitter's developer site and getting all the secret codes you need check out [Gray's tutorial](http://iag.me/socialmedia/how-to-create-a-twitter-app-in-8-easy-steps/).
+
+Next you'll also need a server running PHP and PHP cURL. Just plop the script and Mallison's PHP Twitter wrapper into the appropriate place on your server such as `/var/www/html/chirpme`.
+
+Once that's done, check that it's working properly by going to `http://www.YourServer.com/chirpme/chirpme.php` or whatever web address you've chosen for your site. If you see the error message for the slash command token (`$msg` variable in the script) then the script is working.
+
+As long as you have everything configured properly on Slack--check out [McCreath's Is It Up tutorial](https://github.com/mccreath/isitup-for-slack/blob/master/docs/TUTORIAL.md) for the basics--the script should just work.
+
+Chirpme is currently configured to display tweets to all users in a given channel. If you'd rather that all team members see tweets privately just remove the following" `"response_type" => "in_channel",`.
+
+Enjoy.
---
+++ chirpme.php
@@ -0,0 +1,87 @@
+<?php
+/** Chirpme is a simple Slack slash command that retrieves the latest tweet of a specified username, 
+and returns that tweet as a URL.  
+The script only works if you supply a valid 
+Twitter username. eg. /chirpme ianpaul
+It also relies on Slack's Twitter app to make the link look nice.
+Credit goes to David McCreath's Is It Up for Slack, 
+and Ian Anderson Gray's First Twitter app 
+of which this script is an amalgam with minor additions by me.
+**/
+
+# This call is for James Mallison's PHP Wrapper for Twitter. 
+#You can find it on Github: https://github.com/J7mbo/twitter-api-php
+# You'll need to include the wrapper in the same folder as this script.
+require_once('TwitterAPIExchange.php');
+
+#These variables are used for the Slack portion of our script. 
+
+$command = $_POST['command'];
+$text = $_POST['text'];
+$token = $_POST['token'];
+
+# This checks the Slack token for the slash command, 
+# and makes sure the request is from your team.
+if($token != '[insert your Slack slash command token here]'){ 
+  $msg = ":squirrel: The token for the slash command doesn't match. We're done here until IT fixes it. Don't worry, Squirrelock is on the case.";
+  die($msg);
+  echo $msg;
+}
+
+/** Set Twitter access tokens here--see: https://dev.twitter.com/apps **/
+$settings = array(
+	'oauth_access_token' => "[insert access token here]",
+	'oauth_access_token_secret' => "[insert access token secret here]",
+	'consumer_key' => "[insert consumer key here]",
+	'consumer_secret' => "[insert consumer secret here]"
+);
+
+#This script uses "GET statuses/user_timeline" from the Twitter API v1.1. 
+#This is the URL format for that portion of the API. 
+$url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+#This line is for the wrapper.
+$requestMethod = "GET";
+#Also for the wrapper. Tells the Twitter API the name of the user we want, 
+#and how many tweets we want.
+$getfield = '?screen_name='.$text.'&count=1';
+
+#More wrapper stuff.
+$twitter = new TwitterAPIExchange($settings);
+# This does all the fancy Twitter authentication and then stashes
+# the returned JSON data from Twitter in an associative array.
+$string = json_decode($twitter->setGetfield($getfield)
+                              ->buildOauth($url, $requestMethod)
+                              ->performRequest(), $assoc = TRUE);
+
+#The error message that gets returned to Slack if there's a problem. 
+
+if($string["errors"][0]["message"]!= "") {echo "*Oops, we ran into an issue. IT has put Squirrelock on the case.* :squirrel: In the meantime, here's what Twitter had to say: "
+.$string[errors][0]["message"].""; exit();}
+
+#This is where we pull the data out of the associative array.
+foreach($string as $items)
+{
+  $reply =  "<https://www.twitter.com/".$items['user']['screen_name']."/status/".$items['id_str']."> \n ";
+
+}
+
+#Now we put the data back into JSON so that Slack will do its styling magic 
+#on the returned link.
+$payload = json_encode(array(
+  "response_type" => "in_channel",
+  "text" => $reply,
+  "unfurl_media" => "true",
+  "unfurl_links" => "true"
+));
+
+# Without the header Slack won't recognize you're sending JSON,
+# and you'll end up with a mess. 
+# See Stack Overflow for more information on that trial: 
+# https://stackoverflow.com/questions/37817971/slack-is-printing-raw-json-instead-of-styled-message
+header('Content-Type: application/json');
+
+#Tell Slack to print the JSON we've sent (Slack handles styling on its own).
+echo $payload;
+
+#we're done here.
+?>
